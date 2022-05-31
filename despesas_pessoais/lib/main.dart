05c20c25d81f0684ed46c590f5384ad0b0d59efc
Eric Cacciatori Bellini <eric.cacciatori@hotmail.com>
2022-05-31 10:10:09 -0300

    modelo de transação criado

--- despesas_pessoais/lib/main.dart
+++ despesas_pessoais/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:despesas_pessoais/models/transactions.dart';
 import 'package:flutter/material.dart';
 
 main() {
@@ -16,15 +17,42 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis de corrida',
+      value: 310.73,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 100.32,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Primeira estrutura'),
-      ),
-      body: const Center(
-        child: Text('Versão Inicial'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Primeira estrutura'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment:
+              CrossAxisAlignment.stretch, //Estica os elementos filhos
+          children: const <Widget>[
+            Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Grafico'),
+            ),
+            Card(
+              elevation: 5,
+              child: Text('Lista de Transações'),
+            ),
+          ],
+        ));
   }
 }
